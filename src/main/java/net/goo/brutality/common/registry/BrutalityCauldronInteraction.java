fb23d1eda0654111956c09e575a49c7333e81f26
Bryan Ong <bryanong0906@gmail.com>
2026-03-02 01:53:58 +0800

    Todo: Fix expunge dupe, sprite the other M.A.X. weapons, also make them actually do stuff
    Resprite and rework RoyalGuardianSword
    
    Signed-off-by: Bryan Ong <bryanong0906@gmail.com>

---
+++ src/main/java/net/goo/brutality/common/registry/BrutalityCauldronInteraction.java
@@ -0,0 +1,9 @@
+package net.goo.brutality.common.registry;
+
+import net.minecraft.core.cauldron.CauldronInteraction;
+
+public class BrutalityCauldronInteractions {
+    public static void bootstrap() {
+        CauldronInteraction.addDefaultInteractions();
+    }
+}
